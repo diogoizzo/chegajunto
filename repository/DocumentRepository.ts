7da99134f0bf92914b9d132b4a311e6e4b4ed59a
diogoizzo <dizzodev@gmail.com>
2023-09-28 17:05:21 -0300

    Ajustando DocumentRepository

--- repository/DocumentRepository.ts
+++ repository/DocumentRepository.ts
@@ -23,32 +23,30 @@ export default class DocumentRepository {
          console.log(error);
       }
    }
-   static async create(
-      name: string,
-      type: string,
-      description: string,
-      uploadedByUserId: string,
-      belongsToPatientId: string,
-      googleDriveId: string,
-      mimeType: string
-   ) {
-      try {
-         const doc = await prisma.document.create({
-            data: {
-               name,
-               type,
-               description,
-               uploadedByUserId,
-               belongsToPatientId,
-               googleDriveId,
-               mimeType
-            }
-         });
-         return doc;
-      } catch (error) {
-         console.log(error);
-      }
-   }
+   // static async create(
+   //    name: string,
+   //    type: string,
+   //    description: string,
+   //    uploadedByUserId: string,
+   //    belongsToPatientId: string,
+   //    mimeType: string
+   // ) {
+   //    try {
+   //       const doc = await prisma.document.create({
+   //          data: {
+   //             name,
+   //             type,
+   //             description,
+   //             uploadedByUserId,
+   //             belongsToPatientId,
+   //             mimeType,
+   //          }
+   //       });
+   //       return doc;
+   //    } catch (error) {
+   //       console.log(error);
+   //    }
+   // }
    static async findById(id: string) {
       try {
          const document = await prisma.document.findUnique({
@@ -107,42 +105,42 @@ export default class DocumentRepository {
          console.log(error);
       }
    }
-   static async updateWithFile(
-      id: string,
-      name: string,
-      type: string,
-      description: string,
-      uploadedByUserId: string,
-      belongsToPatientId: string,
-      mimeType: string,
-      googleDriveId: string
-   ) {
-      try {
-         const doc = await prisma.document.update({
-            where: {
-               id: id
-            },
-            data: {
-               name: name,
-               type: type,
-               description: description,
-               uploadedBy: {
-                  connect: { id: uploadedByUserId }
-               },
-               belongsTo: {
-                  connect: {
-                     id: belongsToPatientId
-                  }
-               },
-               googleDriveId: googleDriveId,
-               mimeType: mimeType
-            }
-         });
-         return doc;
-      } catch (error) {
-         console.log(error);
-      }
-   }
+   // static async updateWithFile(
+   //    id: string,
+   //    name: string,
+   //    type: string,
+   //    description: string,
+   //    uploadedByUserId: string,
+   //    belongsToPatientId: string,
+   //    mimeType: string,
+   //    googleDriveId: string
+   // ) {
+   //    try {
+   //       const doc = await prisma.document.update({
+   //          where: {
+   //             id: id
+   //          },
+   //          data: {
+   //             name: name,
+   //             type: type,
+   //             description: description,
+   //             uploadedBy: {
+   //                connect: { id: uploadedByUserId }
+   //             },
+   //             belongsTo: {
+   //                connect: {
+   //                   id: belongsToPatientId
+   //                }
+   //             },
+   //             googleDriveId: googleDriveId,
+   //             mimeType: mimeType
+   //          }
+   //       });
+   //       return doc;
+   //    } catch (error) {
+   //       console.log(error);
+   //    }
+   // }
    static async newThisMonth(initialDate: any, endDate: any) {
       try {
          const newThisMonth = await prisma.document.findMany({
--- services/AwsServices.ts
+++ services/AwsServices.ts
@@ -1,21 +1,20 @@
-import { randomUUID } from 'crypto';
 import s3 from 'aws-sdk/clients/s3';
-
-const region = process.env.REGION;
-const bucketName = process.env.BUCKET_NAME;
-const accessKeyId = process.env.ACCESS_KEY;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-
-const bucket = new s3({
-   apiVersion: '2006-03-01',
-   region,
-   accessKeyId,
-   secretAccessKey,
-   signatureVersion: 'v4'
-});
 
 export default class AwsServices {
    static async generateUrl(awsFileName: string) {
+      const region = process.env.REGION;
+      const bucketName = process.env.BUCKET_NAME;
+      const accessKeyId = process.env.ACCESS_KEY;
+      const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+      const bucket = new s3({
+         apiVersion: '2006-03-01',
+         region,
+         accessKeyId,
+         secretAccessKey,
+         signatureVersion: 'v4'
+      });
+
       const params = {
          Bucket: bucketName,
          Key: awsFileName,
@@ -26,6 +25,19 @@ export default class AwsServices {
       return url;
    }
    static async delete(awsFileName: string) {
+      const region = process.env.REGION;
+      const bucketName = process.env.BUCKET_NAME;
+      const accessKeyId = process.env.ACCESS_KEY;
+      const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+      const bucket = new s3({
+         apiVersion: '2006-03-01',
+         region,
+         accessKeyId,
+         secretAccessKey,
+         signatureVersion: 'v4'
+      });
+
       const deleted = await bucket
          .deleteObject({ Bucket: String(bucketName), Key: awsFileName })
          .promise();
